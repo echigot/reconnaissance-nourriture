38957c98d0404c82a64e259e76c418b50a15fa7f
etienne <etienne@Shodan-MacBook.local>
2021-03-21 16:19:23 +0100

    AmeriqueNord+AmeriqueCentrale à jour

--- nourriture/rename.sh
+++ nourriture/rename.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cd AmeriqueNord
+cd train/AmeriqueCentrale
 k=1
 for i in *.jpg
 do
     echo " $i "
-    new=$(printf "AmeriqueNord.%d.jpg" "$k")
+    new=$(printf "ameriquecentrale%d.jpg" "$k")
     mv "$i" "$new"
     k=$(($k+1))
 done
